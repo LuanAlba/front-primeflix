5eec2f37ba0e147c16091ea7c89158dc8e5ed6e1
LuanAlba <luan.alba2@gmail.com>
2025-06-26 16:17:56 -0300

    Listando os filmes na home// importação axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import api from "../../services/api";
+import './home.css';
+
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+
+        async function loadFilmes() {
+            const response = await api.get("movie/now_playing", {
+                params: {
+                    api_key: "6cb4974c204511a10743cc0c18b32703",
+                    language: "pt-BR",
+                    page: 1,
+                }
+            })
+
+            //guardando o json no useState
+            setFilmes(response.data.results.slice(0,10));
+        }
+
+        loadFilmes();
+
+    }, [])
+
     return (
-        <div>
-            <h1>HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article ket={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     );
 }
