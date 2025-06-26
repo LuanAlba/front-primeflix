2ce6e286758a6641e674cbe635cdd1119ab00c0f
LuanAlba <luan.alba2@gmail.com>
2025-06-26 16:39:03 -0300

    Loading da home e pag. not found

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import './home.css';
 
 function Home() {
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
 
@@ -19,28 +20,37 @@ function Home() {
             })
 
             //guardando o json no useState
-            setFilmes(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0, 10));
+            setLoading(false);
         }
 
         loadFilmes();
 
     }, [])
 
-    return (
-        <div className="container">
-            <div className="lista-filmes">
-                {filmes.map((filme) => {
-                    return(
-                        <article ket={filme.id}>
-                            <strong>{filme.title}</strong>
-                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
-                        </article>
-                    )
-                })}
+    if (loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
             </div>
-        </div>
-    );
+        )
+    }
+
+        return (
+            <div className="container">
+                <div className="lista-filmes">
+                    {filmes.map((filme) => {
+                        return (
+                            <article ket={filme.id}>
+                                <strong>{filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })}
+                </div>
+            </div>
+        );
 }
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
-
+import Erro from "./pages/Erro";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 
 function RoutesApp() {
     return (
         <BrowserRouter>
-            <Header/>
+            <Header />
             <Routes>
-                <Route path="/" element={ <Home/>}/>
-                <Route path="/filme/:id" element={ <Filme/>}/>
+                <Route path="/" element={<Home />} />
+                <Route path="/filme/:id" element={<Filme />} />
+
+                <Route path="*" element={<Erro />} />
             </Routes>
         </BrowserRouter>
     );
