283313d5d16b822a83ddf70f7d652959c0167d0d
LuanAlba <luan.alba2@gmail.com>
2025-06-26 17:41:45 -0300

    Página de detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,63 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+import './filme-info.css'
+
 function Filme() {
+    const { id } = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "6cb4974c204511a10743cc0c18b32703",
+                    language: "pt-BR",
+                }
+            })
+                .then((response) => {
+                    setFilme(response.data);
+                    setLoading(false);
+                })
+                .catch(() => {
+                    console.log("FILME NAO ENCONTRADO")
+                })
+        }
+
+        loadFilme();
+
+        return () => {
+            console.log("COMPONENTE FOI DESMONTADO")
+        }
+    }, [])
+
+    if (loading) {
+        return (
+            <div className="filme-info">
+                <h2>Carregando detalhes...</h2>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h1>FILME</h1>
+        <div className="filme-info">
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strong>Avaliação: {filme.vote_average.toFixed(1)} / 10</strong>
+
+            <div className="area-buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="#">
+                        Trailer
+                    </a>
+                </button>
+            </div>
+
         </div>
     );
 }
