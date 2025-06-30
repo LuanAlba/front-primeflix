40620226a1c04d61764176c83b0161f301c9acae
LuanAlba <luan.alba2@gmail.com>
2025-06-30 11:48:09 -0300

    Navegação para home caso nao encontre o filme pelo ID e navegação p/ trailer do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import api from '../../services/api';
 import './filme-info.css'
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [filme, setFilme] = useState({});
     const [loading, setLoading] = useState(true);
 
@@ -21,7 +22,8 @@ function Filme() {
                     setLoading(false);
                 })
                 .catch(() => {
-                    console.log("FILME NAO ENCONTRADO")
+                    navigate("/", { replace: true });
+                    return;
                 })
         }
 
@@ -30,7 +32,7 @@ function Filme() {
         return () => {
             console.log("COMPONENTE FOI DESMONTADO")
         }
-    }, [])
+    }, [navigate, id])
 
     if (loading) {
         return (
@@ -52,7 +54,7 @@ function Filme() {
             <div className="area-buttons">
                 <button>Salvar</button>
                 <button>
-                    <a href="#">
+                    <a target="_blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer
                     </a>
                 </button>
